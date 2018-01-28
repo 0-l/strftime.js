818be594acbcd6698edf265fc76146b654bba9fa
Bruno Coimbra <b-coimbra@hotmail.com>
2018-01-28 17:33:19 -0200

    minor tweaks

--- strftime.js
+++ strftime.js
@@ -20,11 +20,11 @@ function strftime(format = 'c') {
         return { 1: 'st', 2: 'nd', 3: 'rd' }[((num = this.toString()).length) > 1 ? parseInt(num.split('')[1]) : num] || 'th';
     };
 
-    let date    = new Date();
-    let month   = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    let days    = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    let result  = [];
-    let formats = {
+    let date    = new Date(),
+        month   = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        days    = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+        result  = [],
+        formats = {
         a: days[date.getDay()].substr(0, 3),
         A: days[date.getDay()],
         w: date.getDay(),
@@ -40,6 +40,7 @@ function strftime(format = 'c') {
         p: date.getHours() >= 12 ? 'PM' : 'AM',
         o: date.getDate().ord(),
         M: date.getMinutes(),
+        i: date.getMinutes().pad(),
         S: date.getSeconds(),
         s: date.getSeconds().pad(),
         f: date.getMilliseconds(),
